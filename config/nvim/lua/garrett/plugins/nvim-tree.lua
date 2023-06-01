6ac8ba34871a0efce97db6339619d248c4640240
Garrett Krohn <gkrohn@kipsu.edu>
2023-06-01 05:51:23 -0500

    added in all of my nvim config

---
+++ config/nvim/lua/garrett/plugins/nvim-tree.lua
@@ -0,0 +1,52 @@
+local setup, nvimtree = pcall(require, "nvim-tree")
+if not setup then
+	return
+end
+
+-- recommended settings from nvim-tree documentation
+vim.g.loaded = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- change color for arrows in tree to light blue
+vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
+
+nvimtree.setup({
+	view = {
+		float = {
+			enable = true,
+			quit_on_focus_loss = true,
+			open_win_config = {
+				relative = "editor",
+				border = "rounded",
+				width = 50,
+				height = 100,
+				row = 1,
+				col = 1,
+			},
+		},
+	},
+	-- change folder arrow icons
+	renderer = {
+		icons = {
+			glyphs = {
+				folder = {
+					arrow_closed = "", -- arrow when folder is closed
+					arrow_open = "", -- arrow when folder is open
+				},
+			},
+		},
+	},
+	-- disable window_picker for
+	-- explorer to work well with
+	-- window splits
+	actions = {
+		open_file = {
+			window_picker = {
+				enable = false,
+			},
+		},
+	},
+	-- 	git = {
+	-- 		ignore = false,
+	-- 	},
+})
