3d88a42492a77c051102b4585b3b46dd0cc2585d
Garrett Krohn <gkrohn@kipsu.edu>
2023-06-06 07:55:52 -0500

    starting php config, added vscode php debug

--- config/nvim/lua/garrett/plugins/dap.lua
+++ config/nvim/lua/garrett/plugins/dap.lua
@@ -1,9 +1,15 @@
-require("dapui").setup()
+local dap = require("dap")
+dap.adapters.php = {
+	type = "executable",
+	command = "node",
+	args = { "/Users/garrettkrohn/code/dap/vscode-php-debug/src/phpDebug.ts" },
+}
 
-require("dap").setup()
-
--- dap.adapters.php = {
--- 	type = "executable",
--- 	command = "node",
--- 	args = {},
--- }
+dap.configurations.php = {
+	{
+		type = "php",
+		request = "launch",
+		name = "Listen for Xdebug",
+		port = 9000,
+	},
+}
