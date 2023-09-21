c89a49e59e9b833a63d6bb5e20bcf5cb29fd644e
Garrett Krohn <gkrohn@kipsu.edu>
2023-09-20 19:28:45 -0500

    few aliases, git ignore, and finish dap keybindings

--- config/nvim/lua/garrett/plugins-setup.lua
+++ config/nvim/lua/garrett/plugins-setup.lua
@@ -197,20 +197,20 @@ return packer.startup(function(use)
 		branch = "master",
 		requires = { { "nvim-lua/plenary.nvim" } },
 		-- Lua
-
-		use({
-			"folke/which-key.nvim",
-			config = function()
-				vim.o.timeout = true
-				vim.o.timeoutlen = 300
-				require("which-key").setup({
-					-- your configuration comes here
-					-- or leave it empty to use the default settings
-					-- refer to the configuration section below
-				})
-			end,
-		}),
 	})
+
+	-- use({
+	-- 	"folke/which-key.nvim",
+	-- 	config = function()
+	-- 		vim.o.timeout = true
+	-- 		vim.o.timeoutlen = 300
+	-- 		require("which-key").setup({
+	-- 			-- your configuration comes here
+	-- 			-- or leave it empty to use the default settings
+	-- 			-- refer to the configuration section below
+	-- 		})
+	-- 	end,
+	-- })
 
 	use({
 		"folke/todo-comments.nvim",
