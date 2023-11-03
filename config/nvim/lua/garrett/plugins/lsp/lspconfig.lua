b24aca8debb2b31f7f725fdc6832dc1da80b003d
Garrett Krohn <gkrohn@kipsu.edu>
2023-11-03 13:08:24 -0500

    added alias and python

--- config/nvim/lua/garrett/plugins/lsp/lspconfig.lua
+++ config/nvim/lua/garrett/plugins/lsp/lspconfig.lua
@@ -96,7 +96,7 @@ lspconfig["tailwindcss"].setup({
 lspconfig["emmet_ls"].setup({
 	capabilities = capabilities,
 	on_attach = on_attach,
-	filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+	filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte", "python" },
 })
 
 -- configure intelephense server (php)
