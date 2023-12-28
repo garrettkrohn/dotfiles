4ace77d5b56150c0a03f60f199e6e6a0c46bbc6e
Garrett Krohn <gkrohn@kipsu.edu>
2023-12-28 12:08:49 -0600

    added lualine signs

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -29,12 +29,52 @@ end
 -- 	},
 -- }
 
+local icons = require("config.icons")
+
+local diagnostics = {
+	"diagnostics",
+	sources = { "nvim_diagnostic" },
+	sections = { "error", "warn", "info", "hint" },
+	symbols = {
+		error = icons.diagnostics.Error,
+		hint = icons.diagnostics.Hint,
+		info = icons.diagnostics.Info,
+		warn = icons.diagnostics.Warn,
+	},
+	colored = true,
+	update_in_insert = false,
+	always_visible = false,
+}
+
+local diff = {
+	"diff",
+	symbols = {
+		added = icons.git.added .. " ",
+		untracked = icons.git.added .. " ",
+		modified = icons.git.changed .. " ",
+		removed = icons.git.deleted .. " ",
+	},
+	colored = true,
+	always_visible = false,
+	source = function()
+		local gitsigns = vim.b.gitsigns_status_dict
+		if gitsigns then
+			return {
+				added = gitsigns.added,
+				modified = gitsigns.changed,
+				removed = gitsigns.removed,
+			}
+		end
+	end,
+}
+
 -- configure lualine with modified theme
 lualine.setup({
 	options = {
 		theme = require("plugins.lualineTheme").theme(),
 	},
 	sections = {
+		lualine_c = { diff, diagnostics },
 		lualine_x = {},
 	},
 })
