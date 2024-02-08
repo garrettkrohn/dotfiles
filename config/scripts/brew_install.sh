6d18730fa9d2c2a93dcc3bc21a3af82cb58cb5aa
Garrett Krohn <gkrohn@kipsu.edu>
2024-02-08 15:39:01 -0600

    added brew install script

---
+++ config/scripts/brew_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+brew install alacritty
+brew install docker
+brew install fnm
+brew install fzf
+brew install git
+brew install jq
+brew install python
+brew install starship
+brew install yabai
+brew install skhd
+brew install zoxide
+brew install tmux
+brew install tmuxinator
+brew install pipenv
+
