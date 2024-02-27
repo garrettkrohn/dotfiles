26034323a4c488565f664bbbb4be4e55254a8df5
Garrett Krohn <gkrohn@MacBook-Pro-7.local>
2024-02-27 09:37:04 -0600

    additions to installation script

--- config/scripts/brew_install.sh
+++ config/scripts/brew_install.sh
@@ -16,13 +16,30 @@ brew install git
 brew install jq
 brew install python
 brew install starship
-brew install yabai
-brew install skhd
 brew install zoxide
 brew install tmux
 brew install tmuxinator
 brew install pipenv
 brew install zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+brew install koekeishiya/formulae/yabai
+brew install koekeishiya/formulae/skhd
+curl -sS https://webi.sh/nerdfont | sh
+brew tap epk/epk
+Brew install npm
+
+
+brew tap homebrew/cask-fonts
+brew search '/font-.*-nerd-font/' | awk '{ print $1 }' | xargs -I{} brew install --cask {} || true
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/jeffreytse/zsh-vi-mode \
+  $ZSH_CUSTOM/plugins/zsh-vi-mode
+
+Brew install lazygit
 
 # install neovim
 brew install neovim
