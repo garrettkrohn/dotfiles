04a4c3ec1240c7680375375a30abfda0786f47bb
Garrett Krohn <gkrohn@MacBook-Pro-7.local>
2024-04-14 19:03:36 -0500

    neotest

---
+++ config/kickstart/lua/plugins/neotest.lua
@@ -0,0 +1,22 @@
+return {
+    -- enabled = false,
+    "nvim-neotest/neotest",
+    dependencies = {
+        "nvim-neotest/nvim-nio",
+        "nvim-lua/plenary.nvim",
+        "antoinemadec/FixCursorHold.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "rcasia/neotest-java"
+    },
+    config = function()
+        require("neotest").setup({
+            adapters = {
+                require("neotest-java")({
+                    ignore_wrapper = true, -- whether to ignore maven/gradle wrapper
+                    junit_jar = "/Users/gkrohn/.local/share/kickstart/neotest-java/junit-platform-console-standalone-1.10.1.jar"
+                    -- default: .local/share/nvim/neotest-java/junit-platform-console-standalone-[version].jar
+                })
+            }
+        })
+    end
+}
