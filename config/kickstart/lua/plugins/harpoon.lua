bff296aede1a87ffd458ef6aab2f029284135605
Garrett Krohn <gkrohn@MacBook-Pro-7.local>
2024-04-14 19:02:52 -0500

    fixed harpoon2!

--- config/kickstart/lua/plugins/harpoon.lua
+++ config/kickstart/lua/plugins/harpoon.lua
@@ -1,8 +1,17 @@
 return {
     'ThePrimeagen/harpoon',
     branch = 'harpoon2',
+    commit = '2e25a13',
     lazy=false,
     dependencies = {
         'nvim-lua/plenary.nvim',
     },
+    config = function()
+        require("harpoon"):setup({
+            settings = {
+                save_on_toggle = true,
+                sync_on_ui_close = true
+            }
+        })
+    end
 }
