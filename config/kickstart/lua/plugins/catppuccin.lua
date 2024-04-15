962092059e936b83259853d16506f74bf8dda16a
Garrett Krohn <gkrohn@MacBook-Pro-7.local>
2024-04-14 19:02:36 -0500

    added catppuccin but disabled

---
+++ config/kickstart/lua/plugins/catppuccin.lua
@@ -0,0 +1,8 @@
+return {
+    "catppuccin/nvim",
+    enabled = false,
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    flavour = "macchiato",
+    transparent_background = true}
