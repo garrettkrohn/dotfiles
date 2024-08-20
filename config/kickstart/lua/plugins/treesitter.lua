5ef823ca80161a42416d78a57307bb41dc08ebae
Garrett Krohn <gkrohn@netspi.com>
2024-08-20 09:00:39 -0500

    fix: treesitter fix with a specific commit

--- config/kickstart/lua/plugins/treesitter.lua
+++ config/kickstart/lua/plugins/treesitter.lua
@@ -5,5 +5,5 @@ return {
     'nvim-treesitter/nvim-treesitter-textobjects',
   },
   build = ':TSUpdate',
-  commit = '97ba59c6f532072e456b3d1f2413560e689c132b'
+  -- commit = '97ba59c6f532072e456b3d1f2413560e689c132b'
 }
