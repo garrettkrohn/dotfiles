735aac7cd5599b7ef00c1e284b78a0577f1eeeb0
Garrett Krohn <gkrohn@netspi.com>
2024-09-23 08:51:32 -0500

    removed the highlight current row

--- config/kickstart/lua/config/autocommands.lua
+++ config/kickstart/lua/config/autocommands.lua
@@ -1,3 +1,8 @@
+-- remove the current line highlight
+vim.api.nvim_create_autocmd('ColorScheme', {
+  command = [[highlight CursorLine guibg=NONE cterm=NONE]],
+})
+
 vim.api.nvim_create_autocmd('BufWritePost', {
   pattern = { 'aerospace.toml' },
   command = '!aerospace reload-config',
@@ -30,3 +35,25 @@ vim.api.nvim_create_autocmd('BufWritePost', {
   end,
   desc = 'Reload the current module on save',
 })
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+  pattern = '*',
+  desc = 'Prevent colorscheme clearing self-defined DAP marker colors',
+  callback = function()
+    -- Reuse current SignColumn background (except for DapStoppedLine)
+    local sign_column_hl = vim.api.nvim_get_hl(0, { name = 'SignColumn' })
+    -- if bg or ctermbg aren't found, use bg = 'bg' (which means current Normal) and ctermbg = 'Black'
+    -- convert to 6 digit hex value starting with #
+    local sign_column_bg = (sign_column_hl.bg ~= nil) and ('#%06x'):format(sign_column_hl.bg) or 'Black'
+    local sign_column_ctermbg = (sign_column_hl.ctermbg ~= nil) and sign_column_hl.ctermbg or 'Black'
+
+    vim.api.nvim_set_hl(0, 'DapStopped', { fg = '#00ff00', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
+    vim.api.nvim_set_hl(0, 'DapStoppedLine', { bg = '#2e4d3d', ctermbg = 'Green' })
+    vim.api.nvim_set_hl(0, 'DapBreakpoint', { fg = '#c23127', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
+    vim.api.nvim_set_hl(0, 'DapBreakpointRejected', { fg = '#888ca6', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
+    vim.api.nvim_set_hl(0, 'DapLogPoint', { fg = '#61afef', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
+  end,
+})
+
+-- reload current color scheme to pick up colors override if it was set up in a lazy plugin definition fashion
+vim.cmd.colorscheme(vim.g.colors_name)
