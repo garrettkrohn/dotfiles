0f5824c92e2235144dc25a8e103cef3eab1affae
Garrett Krohn <gkrohn@netspi.com>
2024-09-23 08:54:24 -0500

    config: new nvim config

--- config/kickstart/lua/plugins/java-nvim.lua
+++ config/kickstart/lua/plugins/java-nvim.lua
@@ -1,51 +1,16 @@
--- old way
--- return {
---     'nvim-java/nvim-java',
---     lazy = true,
---     dependencies = {
---         'nvim-java/lua-async-await',
---         'nvim-java/nvim-java-core',
---         'nvim-java/nvim-java-test',
---         'nvim-java/nvim-java-dap',
---         'MunifTanjim/nui.nvim',
---         'neovim/nvim-lspconfig',
---         'mfussenegger/nvim-dap',
---         {
---             'williamboman/mason.nvim',
---             opts = {
---                 registries = {
---                     'github:nvim-java/mason-registry',
---                     'github:mason-org/mason-registry',
---                 }
---             }
---         },
---         {
---             "williamboman/mason-lspconfig.nvim",
---             opts = {
---                 handlers = {
---                     ["jdtls"] = function()
---                         require("java").setup()
---                         require("lspconfig").jdtls.setup({})
---                     end,
---                 },
---             },
---         },
---     },
--- }
---
-
-
--- new way?
 return {
-    "nvim-java/nvim-java",
-    lazy = false,
-    opts = {
-        java_debug_adapter = {
-            enabled = true
-        }
+  'nvim-java/nvim-java',
+  lazy = false,
+  opts = {
+    java_debug_adapter = {
+      enabled = true,
+      notifications = {
+        dap = false,
+      },
     },
-    config = function()
-        require('java').setup()
-        require('lspconfig').jdtls.setup({})
-    end
+  },
+  config = function()
+    require('java').setup()
+    require('lspconfig').jdtls.setup {}
+  end,
 }
