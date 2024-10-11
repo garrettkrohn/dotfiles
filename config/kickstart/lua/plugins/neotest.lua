6c5490bdb015072f48f064e0028f93f4ea5a9cbd
Garrett Krohn <gkrohn@netspi.com>
2024-10-10 20:51:34 -0500

    neotest

--- config/kickstart/lua/plugins/neotest.lua
+++ config/kickstart/lua/plugins/neotest.lua
@@ -1,35 +1,40 @@
 return {
-  {
-    lazy = false,
-    'rcasia/neotest-java',
-    init = function()
-      -- override the default keymaps.
-      -- needed until neotest-java is integrated in LazyVim
-      -- local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      -- run test file
-      -- keys[#keys + 1] = {"<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, mode = "n" }
-      -- -- run nearest test
-      -- keys[#keys + 1] = {"<leader>tr", function() require("neotest").run.run() end, mode = "n" }
-      -- -- debug test file
-      -- keys[#keys + 1] = {"<leader>tD", function() require("neotest").run.run({ strategy = "dap" }) end, mode = "n" }
-      -- -- debug nearest test
-      -- keys[#keys + 1] = {"<leader>td", function() require("neotest").run.run({ vim.fn.expand("%"), strategy = "dap" }) end, mode = "n" }
-    end,
-  },
-  {
-    'nvim-neotest/neotest',
-    dependencies = {
-      'nvim-neotest/nvim-nio',
-      'nvim-lua/plenary.nvim',
-      'antoinemadec/FixCursorHold.nvim',
-      'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  'nvim-neotest/neotest',
+  dependencies = {
+    'marilari88/neotest-vitest',
+    {
+      'fredrikaverpil/neotest-golang',
+      lazy = false,
     },
-    opts = {
-      adapters = {
-        ['neotest-java'] = {
-          -- config here
-        },
+  },
+  opts = {
+    adapters = {
+      ['neotest-vitest'] = {},
+      ['neotest-golang'] = {
+        -- FIX: doesn't work when in subdirectory of repo (ex: "sesh/v2/namer")
+        args = { '-coverprofile=' .. vim.fn.getcwd() .. '/coverage.out' },
       },
+    },
+  },
+  keys = {
+    { '<leader>ta', "<cmd>lua require('neotest').run.attach()<cr>", desc = 'Attach to the nearest test' },
+    { '<leader>tl', "<cmd>lua require('neotest').run.run_last()<cr>", desc = 'Toggle Test Summary' },
+    { '<leader>to', "<cmd>lua require('neotest').output_panel.toggle()<cr>", desc = 'Toggle Test Output Panel' },
+    { '<leader>tp', "<cmd>lua require('neotest').run.stop()<cr>", desc = 'Stop the nearest test' },
+    { '<leader>ts', "<cmd>lua require('neotest').summary.toggle()<cr>", desc = 'Toggle Test Summary' },
+    { '<leader>tt', "<cmd>lua require('neotest').run.run()<cr>", desc = 'Run the nearest test' },
+    {
+      '<leader>tT',
+      "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>",
+      desc = 'Run test the current file',
+    },
+    {
+      '<leader>td',
+      function()
+        require('neotest').run.run { suite = false, strategy = 'dap' }
+      end,
+      desc = 'Debug nearest test',
     },
   },
 }
