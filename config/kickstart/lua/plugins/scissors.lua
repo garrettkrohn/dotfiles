c62126bc7ce0d3bb21ec305e94c34516c68add81
Garrett Krohn <gkrohn@netspi.com>
2024-10-10 20:51:56 -0500

    disable scissors

--- config/kickstart/lua/plugins/scissors.lua
+++ config/kickstart/lua/plugins/scissors.lua
@@ -1,5 +1,6 @@
 return {
   'chrisgrieser/nvim-scissors',
+  enabled = false,
   dependencies = 'nvim-telescope/telescope.nvim', -- optional
   opts = { snippetDir = vim.fn.stdpath 'data' .. '/lazy/friendly-snippets' },
 }
