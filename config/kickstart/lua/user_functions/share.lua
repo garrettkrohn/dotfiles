3dcb55d26f93a74cc29a20f84ed2e3e51159ae29
Garrett Krohn <gkrohn@netspi.com>
2024-10-16 10:39:24 -0500

    added a share function to change background and font size

---
+++ config/kickstart/lua/user_functions/share.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+local share = false
+
+function M.toggleShare()
+  local stdout = vim.loop.new_tty(1, false)
+  if stdout ~= nil then
+    -- Invert the share variable
+    share = not share
+    -- Convert the boolean to a string that can be passed to the stdout:write function
+    local shareStr = share and 'true' or 'false'
+    -- Encode the shareStr to base64
+    local encodedShareStr = vim.fn.system({ 'base64' }, shareStr)
+    -- Write the encodedShareStr to stdout
+    stdout:write(('\x1bPtmux;\x1b\x1b]1337;SetUserVar=%s=%s\b\x1b\\'):format('SHARE', encodedShareStr))
+    vim.cmd [[redraw]]
+  end
+end
+
+vim.cmd 'command! Share lua require("user_functions.share").toggleShare()'
+
+return M
