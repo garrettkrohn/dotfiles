dfdfd909b5eaf96da9374a18fd278034a1b24802
Garrett Krohn <garrettkrohn@gmail.com>
2024-11-08 14:25:38 -0600

    config: switch blink in for nvim-cmp

--- config/kickstart/lua/plugins/lspconfig.lua
+++ config/kickstart/lua/plugins/lspconfig.lua
@@ -5,6 +5,7 @@ return {
     -- Automatically install LSPs to stdpath for neovim
     { 'williamboman/mason.nvim', config = true },
     'williamboman/mason-lspconfig.nvim',
+    'saghen/blink.cmp',
 
     -- Useful status updates for LSP
     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
@@ -13,4 +14,11 @@ return {
     -- Additional lua configuration, makes nvim stuff amazing!
     'folke/neodev.nvim',
   },
+  config = function(_, opts)
+    local lspconfig = require 'lspconfig'
+    for server, config in pairs(opts.servers or {}) do
+      config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+      lspconfig[server].setup(config)
+    end
+  end,
 }
