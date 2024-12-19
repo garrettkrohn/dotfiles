923ba29352af424385b17fdd7d58e70da33741c2
Garrett Krohn <garrettkrohn@gmail.com>
2024-12-19 09:31:59 -0600

    remove neotest

--- config/kickstart/lua/plugins/neotest.lua
+++ config/kickstart/lua/plugins/neotest.lua
@@ -18,23 +18,33 @@ return {
     },
   },
   keys = {
-    { '<leader>ta', "<cmd>lua require('neotest').run.attach()<cr>", desc = 'Attach to the nearest test' },
-    { '<leader>tl', "<cmd>lua require('neotest').run.run_last()<cr>", desc = 'Toggle Test Summary' },
-    { '<leader>to', "<cmd>lua require('neotest').output_panel.toggle()<cr>", desc = 'Toggle Test Output Panel' },
-    { '<leader>tp', "<cmd>lua require('neotest').run.stop()<cr>", desc = 'Stop the nearest test' },
-    { '<leader>ts', "<cmd>lua require('neotest').summary.toggle()<cr>", desc = 'Toggle Test Summary' },
-    { '<leader>tt', "<cmd>lua require('neotest').run.run()<cr>", desc = 'Run the nearest test' },
-    {
-      '<leader>tT',
-      "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>",
-      desc = 'Run test the current file',
-    },
-    {
-      '<leader>td',
-      function()
-        require('neotest').run.run { suite = false, strategy = 'dap' }
-      end,
-      desc = 'Debug nearest test',
-    },
+    -- { '<leader>ta', "<cmd>lua require('neotest').run.attach()<cr>", desc = 'Attach to the nearest test' },
+    -- { '<leader>tl', "<cmd>lua require('neotest').run.run_last()<cr>", desc = 'Toggle Test Summary' },
+    -- { '<leader>to', "<cmd>lua require('neotest').output_panel.toggle()<cr>", desc = 'Toggle Test Output Panel' },
+    -- { '<leader>tp', "<cmd>lua require('neotest').run.stop()<cr>", desc = 'Stop the nearest test' },
+    -- { '<leader>ts', "<cmd>lua require('neotest').summary.toggle()<cr>", desc = 'Toggle Test Summary' },
+    -- { '<leader>tt', "<cmd>lua require('neotest').run.run()<cr>", desc = 'Run the nearest test' },
+    -- {
+    --   '<leader>tT',
+    --   "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>",
+    --   desc = 'Run test the current file',
+    -- },
+    -- {
+    --   '<leader>td',
+    --   function()
+    --     require('neotest').run.run { suite = false, strategy = 'dap' }
+    --   end,
+    --   desc = 'Debug nearest test',
+    -- },
+  },
+  cmd = {
+    'NeotestRun',
+    'NeotestAttach',
+    'NeotestRunLast',
+    'NeotestOutputPanelToggle',
+    'NeotestStop',
+    'NeotestSummaryToggle',
+    'NeotestRunFile',
+    'NeotestDebug',
   },
 }
