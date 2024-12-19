409c9d6ff28b7b822c060f9c9c2fd0bb7659f16f
Garrett Krohn <garrettkrohn@gmail.com>
2024-12-19 09:32:08 -0600

    switch lsp to basedpyright

--- config/kickstart/lua/config/lsp-setup.lua
+++ config/kickstart/lua/config/lsp-setup.lua
@@ -56,7 +56,8 @@ require('mason-lspconfig').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  pyright = {},
+  -- pyright = {},
+  basedpyright = {},
   tsserver = {},
   html = { filetypes = { 'html', 'twig', 'hbs' } },
 
