425876884ac386b3a50c56003b8a497242262cbb
Garrett Krohn <garrettkrohn@gmail.com>
2024-12-28 06:52:00 -0600

    config copilot

--- config/kickstart/lua/plugins/copilot.lua
+++ config/kickstart/lua/plugins/copilot.lua
@@ -17,15 +17,14 @@ end
 return {
   {
     'CopilotC-Nvim/CopilotChat.nvim',
-    branch = 'canary',
     cmd = 'CopilotChat',
     opts = function()
       local user = vim.env.USER or 'User'
       user = user:sub(1, 1):upper() .. user:sub(2)
       return {
-        model = 'gpt-4',
+        model = 'gpt-4o',
         auto_insert_mode = true,
-        show_help = false,
+        show_help = true,
         question_header = '  ' .. user .. ' ',
         answer_header = '  Copilot ',
         window = {
