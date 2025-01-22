caf022d46fdd148d73ee5272fc2a942e359a8aee
Garrett Krohn <garrettkrohn@gmail.com>
2025-01-22 13:25:38 -0600

    config: pyrun

---
+++ config/kickstart/lua/plugins/pyrun.lua
@@ -0,0 +1,14 @@
+-- return {
+--   {
+--     dir = '~/code/neovim_plugins/pyrun.nvim',
+--     lazy = false,
+--     config = function()
+--       require 'pyrun'
+--     end,
+--   },
+-- }
+--
+return {
+  'garrettkrohn/pyrun.nvim',
+  lazy = false,
+}
