e4b955d18eef46628520aea203a0919741d2b1f1
Garrett Krohn <garrettkrohn@gmail.com>
2025-01-28 14:30:16 -0600

    bring back the lsp setup

--- config/kickstart/init.lua
+++ config/kickstart/init.lua
@@ -18,11 +18,15 @@ require 'config.keymaps'
 -- (syntax parser for highlighting)
 require 'config.treesitter-setup'
 
+-- [[ Configure LSP ]]
+-- (Language Server Protocol)
+require 'config.lsp-setup'
+
 require 'config.autocommands'
 
 require 'config.snippets'
 
 -- load user functions
 for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath 'config' .. '/lua/user_functions', [[v:val =~ '\.lua$']])) do
-    require('user_functions.' .. file:gsub('.lua$', ''))
+  require('user_functions.' .. file:gsub('.lua$', ''))
 end
