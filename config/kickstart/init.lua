acc88ad01ab6bee3d224baac5036644341c87533
Garrett Krohn <garrettkrohn@gmail.com>
2025-01-29 20:55:46 -0600

    rearrange init

--- config/kickstart/init.lua
+++ config/kickstart/init.lua
@@ -1,32 +1,16 @@
---b NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+--  _____        __      ___
+-- |  __ \       \ \    / (_)
+-- | |  | |_ __   \ \  / / _ _ __ ___
+-- | |  | | '__|   \ \/ / | | '_ ` _ \
+-- | |__| | |_      \  /  | | | | | | |
+-- |_____/|_(_)      \/   |_|_| |_| |_|
 
--- [[ Install `lazy.nvim` plugin manager ]]
-require 'config.lazy-bootstrap'
-
--- [[ Configure plugins ]]
-require 'config.lazy-plugins'
-
--- [[ Setting options ]]
 require 'config.options'
-
--- [[ Basic Keymaps ]]
+require 'config.lazy-bootstrap'
+require 'config.lazy-plugins'
 require 'config.keymaps'
-
--- [[ Configure Treesitter ]]
--- (syntax parser for highlighting)
 require 'config.treesitter-setup'
-
--- [[ Configure LSP ]]
--- (Language Server Protocol)
 require 'config.lsp-setup'
-
 require 'config.autocommands'
-
 require 'config.snippets'
-
--- load user functions
-for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath 'config' .. '/lua/user_functions', [[v:val =~ '\.lua$']])) do
-  require('user_functions.' .. file:gsub('.lua$', ''))
-end
+require 'config.user_functions'
---
+++ config/kickstart/lua/config/user_functions.lua
@@ -0,0 +1,4 @@
+-- load user functions
+for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath 'config' .. '/lua/user_functions', [[v:val =~ '\.lua$']])) do
+    require('user_functions.' .. file:gsub('.lua$', ''))
+end
